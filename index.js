b8181e3608caa85492dfa9a44ef5915254772195
Wahidul30 <wahidul505@gmail.com>
2022-06-10 12:37:19 +0600

    allowed access from anywhere

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use(cors({
+    origin: '*'
+}));
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bs9or.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
