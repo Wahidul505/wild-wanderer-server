4d28594c144fe5b90efaa3f7af74c1d509be2721
Wahid <macbookair@MacBooks-MacBook-Air.local>
2023-04-30 14:03:22 +0600

    updated

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors({
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bs9or.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://wild-wanderer-admin:JqOQiezRZZcdDPvB@cluster0.bs9or.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
